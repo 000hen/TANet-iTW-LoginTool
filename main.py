11de0c4b03c414eb01cdc870b501be00281229af
Muisnow <42735043+000hen@users.noreply.github.com>
2021-10-29 22:10:37 +0800

    修復了一些網址轉換的問題, 順便讓他在無法登入時 自動重新登入

--- main.py
+++ main.py
@@ -8,21 +8,30 @@ iTaiwansPassword = "xxxxxxxxxxxxx" #此手機號的iTaiwan密碼
 
 warnings.filterwarnings("ignore")
 
-site = "http://172.217.160.110/"   #這是測試是否要登入
+site = "http://clients1.google.com/generate_204"   #這是測試是否要登入
 authdata = {"username": iTaiwansPhoneNumber + "@itw", "password": iTaiwansPassword, "popup": "true"}
-respond = requests.get(site, verify=False)
-if respond.url != site:
-    logurl = urlparse(respond.url).query
-    logurl = parse_qs(logurl, keep_blank_values=True)
-    logurl = logurl["loginurl"][0]
-    print(logurl)
+def logPst(url):
     ot2 = requests.post(logurl, data=authdata, verify=False)
     if ot2.status_code == 200:
-        soup = BeautifulSoup(ot.text, 'html.parser')
+        soup = BeautifulSoup(ot2.text, 'html.parser')
         status = soup.find("font", {"color" : "red"}).text
         if status == "無線上網認證成功！":
             print("Auth Successful!")
+            return False
         else:
-            print("Auth Failed!")
+            print("Auth Failed! Trying To Login...")
+            return True
+
+respond = requests.get(site, verify=False)
+if respond.url != site:
+    print("Needs To Login")
+    logurl = urlparse(respond.url).query
+    logurl = parse_qs(logurl, keep_blank_values=True)
+    logurl = logurl["loginurl"][0]
+    print("Found Login URL: {}, Trying To Login...".format(logurl))
+    h = True
+    while h:
+        h = logPst(logurl)
+        # time.sleep(1)
 else:
     print("Not Needs To Auth")
